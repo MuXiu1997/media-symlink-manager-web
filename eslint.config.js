331a9679bab45780013dbf3c4c573d329b6f9ab5
MuXiu1997 <muxiu1997@gmail.com>
2023-11-27 00:45:53 +0800

    Add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,57 @@
 import antfu from '@antfu/eslint-config'
 import unocss from '@unocss/eslint-plugin'
 
-export default antfu(
+/** @type {import('@antfu/eslint-config').UserConfigItem[]} */
+const overrides = [
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.vue'],
+    rules: {
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'index',
+            'sibling',
+            'parent',
+            'internal',
+            'external',
+            'builtin',
+            'object',
+            'unknown',
+            'type',
+          ],
+          'newlines-between': 'always',
+          'alphabetize': {
+            order: 'asc',
+          },
+        },
+      ],
+      'curly': [
+        'error',
+        'multi-line',
+        'consistent',
+      ],
+      'antfu/if-newline': [
+        'off',
+      ],
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 1,
+          multiline: 1,
+        },
+      ],
+    },
+  },
+]
+
+export default await antfu(
   {},
+  ...overrides,
   unocss.configs.flat,
 )
